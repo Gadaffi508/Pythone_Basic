fd20bcdf656bfe90992673cfb0c850479982db89
Yusuf <yusuf_zeki1234@hotmail.com>
2023-04-19 14:40:31 +0300

    New work added

---
+++ firstday.py
@@ -0,0 +1,21 @@
+print("Hello Yusuf")
+print("Hello Yusuf \nCode start")
+print("C# " + "pythone")
+#input() will get user input in console
+#then print() will print the world "Hello" and the user input
+input("What is your name")
+print("Hello " +input("What is your name ? "))
+print(len("Yusuf"))
+print(len(input("What is your name ? ")))
+name = input("What is your name")
+length = len(name)
+print(length)
+#1. create a greeting for  your program.
+print("Welcome to the band name generator.")
+#2. ask the user for the city that they grew up in.
+city = input("which city did you grow up in ? \n")
+#3. ask the user for the name of a pet
+pet = input("what is the name of a pet ? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
+#5. make sure the input cursor shows on a new line, see the example at:  
