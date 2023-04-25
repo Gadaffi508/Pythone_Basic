3a419e53f9cd8042648121549124b80088ea7777
Yusuf <yusuf_zeki1234@hotmail.com>
2023-04-25 14:29:33 +0300

    New Work Added

--- thirdday.py
+++ thirdday.py
@@ -93,3 +93,55 @@ if add_pepperon == "Y":
 if extra_chesse == "Y":
     bill +=1
 print(f"Your final bill is ${bill}")
+
+# Don't change the code below
+print("Welcome to the love Calculator!")
+name1 = input("What is your name ? \n")
+name2 = input("What is their name ? \n")
+# Don't change the code above
+
+# Write your code below this line
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+truee= t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+love_score = int(str(truee) + str(love))
+if love_score < 10 or love_score > 90 :
+    print(f"Your love score is {love_score}, you go together like coke and mentos")
+elif love_score >= 40 and love_score <= 50 :
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}")
+
+
+print("Welcome to Tresure  Island.")
+print("Your mission is to find treasure.")
+choice1 = input('You\'re at a crossroad, where do you want to go ? Type "left" or "right".\n').lower()
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. there is an island in the middle of the lake. Type "wait" to wait for a boat.Type "swim" to  swim across').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and onw blue. Which colour do you choose ? ")
+        if choice3 == "red":
+            print("It's a room full o fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over")
+        else :
+            print("You chose a door that doesn't exist. Game Over")
+    else:
+        print("You got attacked by an angry trout. Game Over")
+else :
+    print("You fell into a hole. Game Over")
