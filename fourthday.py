d773cd48ec894bd09822203911597cbb25dcdb92
Yusuf <yusuf_zeki1234@hotmail.com>
2023-04-27 09:58:58 +0300

    Fourth day Finished

--- fourthday.py
+++ fourthday.py
@@ -50,3 +50,26 @@ vertical = int(position[1])
 
 selected_row = map[vertical-1]
 selected_row[horizontal-1] = "X"
+
+Rock = "Taş"
+
+Paper = "Kağit"
+
+Scissors = "Makas"
+
+user_choice = int(input("What do you choose ? Type 0 for Rock, 1 for Paper or 2 for Scissors \n"))
+
+computer_choice = random.randint(0,2)
+
+if user_choice>=3 or user_choice<0 :
+    print("You typed an invalid number, you lose")
+elif user_choice == 0 and computer_choice == 2:
+    print("You Win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You Lose!")
+elif computer_choice<user_choice:
+    print("You Win!")
+elif computer_choice>user_choice:
+    print("You Lose!")
+elif computer_choice == user_choice:
+    print("It's a draw")
