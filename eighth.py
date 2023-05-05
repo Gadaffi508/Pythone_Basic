11b19a1d01cb26a0f9aecf3057ee970ab32ad07d
Yusuf <yusuf_zeki1234@hotmail.com>
2023-05-05 11:37:05 +0300

    eightday updated

--- eighth.py
+++ eighth.py
@@ -37,3 +37,39 @@ def prime_checker(number):
 
 n = int(input("Check this number : "))
 prime_checker(number = n)
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','y','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','y','w','x','y','z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt : \n")
+
+text = input("Type your message: \n ").lower()
+shift = int(input("Type the shift number : \n"))
+
+def ceaser(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    for letter in start_text:
+        position = alphabet.index(letter)
+        if cipher_direction == "decode":
+            shift_amount *= -1
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction} text is {end_text}")
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        poisition = alphabet.index(letter)
+        new_position = poisition + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+def decrypt(cipher_text,shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+
+ceaser(start_text=text,shift_amount=shift,cipher_direction=direction)
